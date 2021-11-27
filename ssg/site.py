d336618cfead2a2a93eee7eff548ae5e719940d0
polyatomica <adamsna@gmail.com>
2021-11-27 13:20:43 -0700

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+Class Site:
+def __init(self,source,dest):
+    self.source  = Path(source)
+    self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relevative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self)
+    self.dest.mkdir(parents=True, exist_ok=True)
+for path in self.source.rglob("*"):
+    if path.is_dir():
+        self.createdir(path)
